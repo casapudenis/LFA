f9ab9bd364d09e115d5ad12593d5676958f3f3b5
Casapu Denis-Cristian <53213273+casapudenis@users.noreply.github.com>
2024-05-17 01:56:53 +0300

    Schema logica

--- Tema2_automat_de_stari/coffee.py
+++ Tema2_automat_de_stari/coffee.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 class CoffeeShopApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Coffee Shop App")
-        self.geometry("400x300")
+        self.geometry("500x400")
 
         self.current_state_index = 0
         self.states = [
@@ -18,6 +19,7 @@ class CoffeeShopApp(tk.Tk):
         ]
 
         self.user_data = {}
+        self.needs_sugar_amount = False
 
         self.create_widgets()
         self.set_initial_state()
@@ -25,7 +27,7 @@ class CoffeeShopApp(tk.Tk):
     def create_widgets(self):
         self.label_text = tk.StringVar()
         self.label_text.set("Bine ati venit la Coffee Shop!")
-        self.label = ttk.Label(self, textvariable=self.label_text, font=("Helvetica", 16))
+        self.label = ttk.Label(self, textvariable=self.label_text, font=("Comic Sans MS", 16))
         self.label.pack(pady=20)
 
         self.option_frame = ttk.Frame(self)
@@ -36,10 +38,6 @@ class CoffeeShopApp(tk.Tk):
 
         self.next_button = ttk.Button(self.button_frame, text="Urmatorul", command=self.next_state)
         self.next_button.pack(side=tk.RIGHT, padx=5)
-
-        self.previous_button = ttk.Button(self.button_frame, text="Anterior", command=self.previous_state)
-        self.previous_button.pack(side=tk.RIGHT, padx=5)
-        self.previous_button.pack_forget()
 
         self.quit_button = ttk.Button(self.button_frame, text="Iesire", command=self.destroy)
         self.quit_button.pack(side=tk.LEFT, padx=5)
@@ -55,20 +53,39 @@ class CoffeeShopApp(tk.Tk):
 
     def update_state_label(self):
         if self.current_state_index == 0:
-            self.previous_button.pack_forget()
             self.label_text.set("Bine ati venit la Coffee Shop!")
         elif self.current_state_index == len(self.states) - 1:
-            self.previous_button.pack(side=tk.RIGHT, padx=5)
             self.label_text.set("Multumim pentru comanda!")
         else:
-            self.previous_button.pack(side=tk.RIGHT, padx=5)
             self.label_text.set("Continuati comanda")
 
     def next_state(self):
+        if self.current_state_index == len(self.states) - 1:
+            return
+
+        if self.current_state_index == self.states.index(self.selectare_adaosuri):
+            if any(item[0] == "Zahar" and item[1].get() == 1 for item in self.user_data.get("adaosuri", [])) and not self.needs_sugar_amount:
+                self.needs_sugar_amount = True
+                self.current_state.pack_forget()
+                self.current_state, has_buttons = self.adaugare_cantitate_zahar()
+                self.current_state.pack(in_=self.option_frame, fill=tk.BOTH, expand=True)
+                self.update_state_label()
+                return
+            else:
+                self.needs_sugar_amount = False
+
+        if self.needs_sugar_amount and self.current_state_index == self.states.index(self.plasare_comanda) - 1:
+            self.needs_sugar_amount = False
+            self.current_state.pack_forget()
+            self.current_state, has_buttons = self.adaugare_cantitate_zahar()
+            self.current_state.pack(in_=self.option_frame, fill=tk.BOTH, expand=True)
+            self.update_state_label()
+            return
+
         if all(self.option_selected(option) for option in self.user_data if not option.endswith("_optional")):
             self.current_state_index += 1
+            self.current_state.pack_forget()
             if self.current_state_index < len(self.states):
-                self.current_state.pack_forget()
                 self.current_state, has_buttons = self.states[self.current_state_index]()
                 self.current_state.pack(in_=self.option_frame, fill=tk.BOTH, expand=True)
                 if has_buttons:
@@ -76,19 +93,8 @@ class CoffeeShopApp(tk.Tk):
                 else:
                     self.button_frame.pack_forget()
                 self.update_state_label()
-
-    def previous_state(self):
-        self.current_state_index -= 1
-        if self.current_state_index < 0:
-            self.current_state_index = 0
-        self.current_state.pack_forget()
-        self.current_state, has_buttons = self.states[self.current_state_index]()
-        self.current_state.pack(in_=self.option_frame, fill=tk.BOTH, expand=True)
-        if has_buttons:
-            self.button_frame.pack()
         else:
-            self.button_frame.pack_forget()
-        self.update_state_label()
+            messagebox.showwarning("Atentie", "Va rugam sa selectati cel putin o optiune.")
 
     def option_selected(self, option):
         if isinstance(self.user_data[option], dict):
@@ -107,7 +113,7 @@ class CoffeeShopApp(tk.Tk):
         options = ["Cafea", "Ceai", "Ciocolata calda"]
         for option in options:
             rb = ttk.Radiobutton(state_frame, text=option, variable=self.user_data["bautura"], value=option)
-            rb.pack(anchor=tk.W)
+            rb.pack(side=tk.TOP, anchor=tk.W)
         return state_frame, True
 
     def selectare_marime(self):
@@ -117,14 +123,15 @@ class CoffeeShopApp(tk.Tk):
 
         self.user_data["marime"] = tk.StringVar()
         options = ["Mica", "Medie", "Mare"]
-        for option in options:
-            rb = ttk.Radiobutton(state_frame, text=option, variable=self.user_data["marime"], value=option)
-            rb.pack(anchor=tk.W)
+        
+        size_dropdown = ttk.Combobox(state_frame, textvariable=self.user_data["marime"], values=options, state="readonly")
+        size_dropdown.pack()
+
         return state_frame, True
 
     def adaugare_arome(self):
         state_frame = ttk.Frame(self)
-        label = ttk.Label(state_frame, text="Adaugati arome (optional):")
+        label = ttk.Label(state_frame, text="Adaugati arome:")
         label.pack()
 
         self.user_data["arome"] = {}
@@ -132,7 +139,7 @@ class CoffeeShopApp(tk.Tk):
         for option in options:
             self.user_data["arome"][option] = tk.IntVar(value=0)
             cb = ttk.Checkbutton(state_frame, text=option, variable=self.user_data["arome"][option])
-            cb.pack(anchor=tk.W)
+            cb.pack(side=tk.TOP, anchor=tk.W)
         return state_frame, True
 
     def selectare_mod_preparare(self):
@@ -144,12 +151,12 @@ class CoffeeShopApp(tk.Tk):
         options = ["Espresso", "Filtru", "Capsula"]
         for option in options:
             rb = ttk.Radiobutton(state_frame, text=option, variable=self.user_data["mod_preparare"], value=option)
-            rb.pack(anchor=tk.W)
+            rb.pack(side=tk.TOP, anchor=tk.W)
         return state_frame, True
 
     def selectare_adaosuri(self):
         state_frame = ttk.Frame(self)
-        label = ttk.Label(state_frame, text="Selectati adaosurile (optional):")
+        label = ttk.Label(state_frame, text="Selectati adaosurile :")
         label.pack()
 
         self.user_data["adaosuri"] = []
@@ -157,9 +164,38 @@ class CoffeeShopApp(tk.Tk):
         for option in options:
             var = tk.IntVar(value=0)
             cb = ttk.Checkbutton(state_frame, text=option, variable=var)
-            cb.pack(anchor=tk.W)
+            cb.pack(side=tk.TOP, anchor=tk.W)
             self.user_data["adaosuri"].append((option, var))
         return state_frame, True
+
+    def adaugare_cantitate_zahar(self):
+        state_frame = ttk.Frame(self)
+        label = ttk.Label(state_frame, text="Introduceti cantitatea de zahar (intre 1 si 10 grame):")
+        label.pack()
+
+        entry_var = tk.StringVar()
+        entry = ttk.Entry(state_frame, textvariable=entry_var)
+        entry.pack()
+
+        def validate_sugar_and_proceed(event=None):
+            sugar_amount = entry_var.get()
+            try:
+                sugar_amount = int(sugar_amount)
+                if 1 <= sugar_amount <= 10:
+                    self.user_data["cantitate_zahar"] = entry_var
+                    self.next_state()
+                else:
+                    messagebox.showwarning("Atentie", "Cantitatea de zahar trebuie sa fie intre 1 si 10 grame!")
+            except ValueError:
+                messagebox.showwarning("Atentie", "Introduceti o valoare numerica pentru cantitatea de zahar!")
+
+        self.next_button.configure(command=validate_sugar_and_proceed)
+        entry.bind("<Return>", validate_sugar_and_proceed)
+
+        self.user_data["cantitate_zahar"] = entry_var
+
+        return state_frame, True
+
 
     def plasare_comanda(self):
         state_frame = ttk.Frame(self)
@@ -180,10 +216,12 @@ class CoffeeShopApp(tk.Tk):
                 if selected_adaosuri:
                     data_label = ttk.Label(state_frame, text=f"Adaosuri: {', '.join(selected_adaosuri)}")
                     data_label.pack()
+            elif key == "cantitate_zahar" and value.get():
+                data_label = ttk.Label(state_frame, text=f"Cantitate Zahar: {value.get()} grame")
+                data_label.pack()
             else:
                 data_label = ttk.Label(state_frame, text=f"{key.replace('_', ' ').title()}: {value.get()}")
                 data_label.pack()
-
         return state_frame, False
 
 
